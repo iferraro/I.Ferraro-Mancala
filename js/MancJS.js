8b3762bd602e5a7fef23a012027f9262e2187603
iferraro <afronerd01@outlook.com>
2021-09-10 15:00:23 -0700

    Several variables + functions + experimetning with CSS

--- js/MancJS.js
+++ js/MancJS.js
@@ -1,30 +1,94 @@
-const player = {
-    store: document.querySelector("#playerStore"),
-    score: 0
+/*----- constants -----*/
+
+const stores = {
+    player: document.querySelector("#playerStore"),
+    computer: document.querySelector("#compStore")
 }
-const computer = {
-    store: document.querySelector("computerStore"),
-    score: 0
+
+const pits = {
+    one: document.querySelector("#one"),
+    two: document.querySelector("#two"),
+    three: document.querySelector("#three"),
+    four: document.querySelector("#four"),
+    five: document.querySelector("#five"),
+    six: document.querySelector("#six"),
+    seven: document.querySelector("#seven"),
+    eight: document.querySelector("#eight"),
+    nine: document.querySelector("#nine"),
+    ten: document.querySelector("#ten"),
+    eleven: document.querySelector("#eleven"),
+    twelve: document.querySelector("#twelve")
+}
+
+const pitOpposites = {
+    one: document.querySelector("#seven"),
+    two: document.querySelector("#eight"),
+    three: document.querySelector("#nine"),
+    four: document.querySelector("#ten"),
+    five: document.querySelector("#eleven"),
+    six: document.querySelector("#twelve"),
+    seven: document.querySelector("#one"),
+    eight: document.querySelector("#two"),
+    nine: document.querySelector("#three"),
+    ten: document.querySelector("#four"),
+    eleven: document.querySelector("#five"),
+    twelve: document.querySelector("#six")  
+}
+
+const[]
+/*----- app's state (variables) -----*/
+let scores;
+let playerTurn;
+let computerTurn;
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+let allPits = document.querySelectorAll(".pit");
+allPits.forEach(function(pit) {
+    pit.addEventListener("mouseenter", function() {
+        this.style.height = "175px";
+    });
+    pit.addEventListener("mouseleave", function() {
+        this.style.height = "125px";
+    });
+});
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", function() {
+    init();
+});
+
+/*----- functions -----*/
+init();
+function init() {
+    scores = {
+        player: 0,
+        computer: 0
+    }
+    for (let pit in pits) {
+        pits[pit].innerText = 4;
+    }
+    playerTurn = true;
+    computerTurn = false;
+    render(); 
 }
 
 function render() {
-// updates model
+    stores.player.innerText = scores.player;
+    stores.computer.innerText = scores.computer;
+    endCheck();
 }
 
-function reset() {
-// resets everything; scores to 0, each pit has four stones
-}
-
-function advance() {
+function advance(player) {
 // player or computer picks up stones and continues collecting and dropping into pits unitl their turn is done
+
 }
 
-function storeCount() {
+function endCheck() {
+    if (stores.player.innerText + stores.computer.innerText === 48) {
+        playerTurn = false;
+        computerTurn = false;
+    }
 // returns sum of stores. if 48, then game is over.
 }
-
-//hover mouse functions for pits
-
-
 
 
