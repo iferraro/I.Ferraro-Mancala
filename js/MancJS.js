e92b8e302d955dd657d0e67ebf7d46e358a7f3d0
iferraro <afronerd01@outlook.com>
2021-09-09 17:53:02 -0700

    General CSS layout

---
+++ js/MancJS.js
@@ -0,0 +1,16 @@
+const player = {
+    store: document.querySelector("#playerStore"),
+    score: Number(document.querySelector("#pScore").innerText)
+}
+const computer = {
+    store: document.querySelector("computerStore"),
+    score: Number(document.querySelector("#cScore").innerText)
+}
+
+// player.store.addEventListener("click", function() {
+//     this.style.backgroundColor = "red";
+//     this.innerText = player.score;
+//     player.score++;
+// });
+
+
