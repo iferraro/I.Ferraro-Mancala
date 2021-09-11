4564d5d12699b3bdf5bac57106754a0537fc9220
iferraro <afronerd01@outlook.com>
2021-09-11 13:38:20 -0700

    Basic distribution function working, winner/loser functions in progress

--- js/MancJS.js
+++ js/MancJS.js
@@ -3,22 +3,6 @@ const stores = {
     player: document.querySelector("#playerStore"),
     computer: document.querySelector("#compStore")
 }
-const sequence = [
-    document.querySelector("#one"),
-    document.querySelector("#two"),
-    document.querySelector("#three"),
-    document.querySelector("#four"),
-    document.querySelector("#five"),
-    document.querySelector("#six"),
-    stores.player,
-    document.querySelector("#twelve"),
-    document.querySelector("#eleven"),
-    document.querySelector("#ten"),
-    document.querySelector("#nine"),
-    document.querySelector("#eight"),
-    document.querySelector("#seven"),
-    stores.computer
-]
 
 /*----- app's state (variables) -----*/
 let scores;
@@ -27,18 +11,18 @@ let scores;
 
 /*----- cached element references -----*/
 const pits = {
-    one: sequence[0],
-    two: sequence[1],
-    three: sequence[2],
-    four: sequence[3],
-    five: sequence[4],
-    six: sequence[5],
-    seven: sequence[12],
-    eight: sequence[11],
-    nine: sequence[10],
-    ten: sequence[9],
-    eleven: sequence[8],
-    twelve: sequence[7]
+    one: document.querySelector("#one"),
+    two: document.querySelector("#two"),
+    three: document.querySelector("#three"),
+    four: document.querySelector("#four"),
+    five: document.querySelector("#five"),
+    six: document.querySelector("#six"),
+    seven: document.querySelector("#seven"),
+    eight: document.querySelector("#eight"),
+    nine: document.querySelector("#nine"),
+    ten: document.querySelector("#ten"),
+    eleven: document.querySelector("#eleven"),
+    twelve: document.querySelector("#twelve")
 }
 
 const pitOpposites = {
@@ -93,14 +77,14 @@ const computerSide = availableToComputer.slice(6, 12);
 
 /*----- event listeners -----*/
 const allPits = document.querySelectorAll(".pit");
-allPits.forEach(function(pit) {
-    pit.addEventListener("mouseenter", function() {
-        this.style.height = "175px";
-    });
-    pit.addEventListener("mouseleave", function() {
-        this.style.height = "125px";
-    });
-});
+// allPits.forEach(function(pit) {
+//     pit.addEventListener("mouseenter", function() {
+//         this.style.height = "175px";
+//     });
+//     pit.addEventListener("mouseleave", function() {
+//         this.style.height = "125px";
+//     });
+// });
 
 const allStores = document.querySelectorAll(".store");
 allStores.forEach(function(store) {
@@ -116,6 +100,14 @@ const resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click", function() {
     init();
 });
+resetButton.addEventListener("mouseenter", function() {
+    this.style.width = "290px";
+    this.style.height = "80px";
+});
+resetButton.addEventListener("mouseleave", function() {
+    this.style.width = "270px";
+    this.style.height = "60px";
+});
 
 /*----- functions -----*/
 init();
@@ -128,6 +120,11 @@ function init() {
     stores.computer.innerText = 0;
     for (let pit in pits) {
         pits[pit].innerText = 4;
+        pits[pit].style.backgroundColor = "white"; // or theme color of choice
+        pits[pit].style.height = "125px";
+    }
+    for (let store in stores) {
+        stores[store].style.backgroundColor = "white";
     }
     winner = null;
     // playerTurn = true;
@@ -136,6 +133,19 @@ function init() {
 }
 
 function render() {
+    // playerSide.forEach(function(pit) {
+    //     if (pit.innerText === 0) {
+    //          computerSide.forEach(function(compPit) {
+    //              stores.computer.innerText += compPit.innerText;
+    //              compPit.innerText = 0;
+    //          });
+    //     }
+    // });
+    // computerSide.forEach(function(pit) {
+    //     if (pit.innerText === 0) {
+    //          gameOver();
+    //     }
+    // });
     if (stores.player.innerText + stores.computer.innerText === 48) {
         if (stores.player.innerText > stores.computer.innerText) {
             winner = "Player";
@@ -150,45 +160,72 @@ function render() {
     }
     for (let pit in pits) {
         if (Number(pits[pit].innerText) === 0) {
-            pits[pit].style.backgroundColor = "darkslategrey";
+            pits[pit].style.backgroundColor = "darkslategrey"; // or theme color of choice
+            pits[pit].style.height = "125px";
+        } else {
+            pits[pit].style.backgroundColor = "white"; // or theme color of choice
         }
     }
 }
 
+// function gameOver() {
+//     if ()
+// }
+
 // if pit.innerText !== 0, allow player/computer to choose it to go
 
-function playerDist(pit) {
+function playerSow(pit) {
     let stonesInHand = pit.innerText;
     let pitInd = availableToPlayer.indexOf(pit) + 1;
+    if (pitInd === availableToPlayer.length) {
+        pitInd = 0;
+    }
     pit.innerText = 0;
     while (stonesInHand > 0) {
         availableToPlayer[pitInd].innerText++;
         stonesInHand--;
-        if (pitInd > 12) {
+        pitInd++;
+        if (pitInd === availableToPlayer.length) {
             pitInd = 0;
         }
-        pitInd++;
     } 
     let finalPit = availableToPlayer[pitInd-1];
-    finalPit.style.backgroundColor = "green";
-    if (finalPit.innerText === 1 && playerSide.includes(finalPit)) {
-        merge(finalPit, stores.player);
+    for (let pit in pits) {
+        if (pits[pit] !== finalPit) {
+            pits[pit].style.height = "125px";
+        }
+    }
+    if (finalPit !== stores.player) {
+        finalPit.style.height = "175px";
+    }
+    if (playerSide.includes(finalPit)) {
+        if (Number(finalPit.innerText) === 1) {
+            merge(finalPit, stores.player);
+        }
+    } else if (finalPit === stores.player) {
+        playerTurn = true;
+    }
+    else {
+        playerTurn = false;
+        computerTurn = true;
     }
     // other conditions for ending turn
     render(); 
 }
 
 function merge(pit, store) {
-    store.innerText += pit;
+    store.innerText += pit.innerText;
     pit.innerText = 0;
     store.innerText += pitOpposites[pit].innerText;        
     pitOpposites[pit].innerText = 0;
 }
-function computerMerge() {
 
+for (let pit in pits) {
+    pits[pit].addEventListener("click", function() {
+        playerSow(this);
+    });
 }
 
-pits.three.addEventListener("click", playerDist(pits.three));
 
 
 
